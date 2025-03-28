9999816f85a3d6e34df06a9ed6d7bd6444c606a8
rickert156 <rickert156@rambler.ru>
2025-03-28 08:30:46 +0400

    complite first step

--- module/processing_base.py
+++ module/processing_base.py
@@ -6,11 +6,14 @@ counter_number = 0
 def ProcessingTXT(base:str):
     number_ru = set()
     number_uk = set()
+    number_by = set()
     
     global counter_number
     with open(base, 'r') as file:
         for info in file.readlines():
             all_data = info.split()
+            if '[' in info:all_data = info.replace('[', ' ')
+            if ']' in info:all_data = info.replace(']', ' ')
             if ':' in info:all_data = info.split(':')
             if '|' in info:all_data = info.split('|')
             phone = None
@@ -35,10 +38,19 @@ def ProcessingTXT(base:str):
                         recordingResult(location='uk', phone=phone)
                         counter_number+=1
                 
+                # номера беларуси
+                if len(data) == 12:
+                    if data[0:3] == '375':
+                        phone = data
+                        number_by.add(phone)
+                        recordingResult(location='be', phone=phone)
+                        counter_number+=1
+                
                 if phone != None:print(f'[{counter_number}] {base}\t{phone}')
     print(
             f'Номеров РФ: {len(number_ru)}\n'
             f'Номеров Украины: {len(number_uk)}\n'
+            f'Номеров Беларусии: {len(number_by)}'
             )
 
 def ProcessingCSV():
